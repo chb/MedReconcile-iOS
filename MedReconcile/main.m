abb91b3becdfeb4752ccfb2d79a37252bbc1d875
Pascal Pfiffner <phase.of.matter@gmail.com>
2011-10-28 21:43:31 -0400

    Initial Commit

---
+++ MedReconcile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MedReconcile
+//
+//  Created by Pascal Pfiffner on 10/28/11.
+//  Copyright (c) 2011 Children's Hospital Boston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "INAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([INAppDelegate class]));
+	}
+}
